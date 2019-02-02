de0a9d5d097f53a01b8995bff2aac86e3343744b
Lizzie Chan <lchan217@gmail.com>
2019-02-02 23:13:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,12 +6,11 @@ class Dog
     
     @@all << name 
   end 
-  def self.all
-    @@all.each do |dog_name|
-      puts dog_name.name #if you don't have name, will return objectID
-    end
-  end
-
+  def self.all 
+    @@all.map do |name|
+      puts name 
+    end 
+  end 
   def self.clear_all
     @@all.clear 
   end 
